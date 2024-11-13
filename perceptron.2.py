f1cbc62f92b4430945f7b2a54302b38dd15f5de5
carlotachocarro <carlota.chocarro@students.salle.url.edu>
2024-11-13 11:42:00 +0100

    primer commit

--- perceptron.2.py
+++ perceptron.2.py
@@ -3,37 +3,57 @@ import numpy as np
 
 alpha = 0.25
 wl1 = np.ones ([3, 2])
-x = np.array ([0, 1])
-y = np.array ([0, 1])
-bl1 = np.ones ([3])
-a1 = np.ones ([3])
+x = np.ones ([2,1])
+y = np.ones ([2,1])
+bl1 = np.ones ([3, 1])
+a1 = np.ones ([3,1])
 wl2 = np.ones([2, 3])
-bl2 = np.ones ([2])
-a2 = np.ones([2])
-zl1 = np.array ([3])
-zl2 = np.array ([2])
-error = np.ones ([2])
+bl2 = np.ones ([2,1])
+a2 = np.ones([2,1])
+zl1 = np.ones ([3,1])
+zl2 = np.ones ([2,1])
+error2 = np.ones ([2,1])
+deltas1 = np.ones ([3, 1])
 errorpesos2 = np.ones ([2, 3])
+errorpesos1 = np.ones ([3, 2])
 
 def sigmoide(z):
-    out = 1 / (1 + math.e**(-z))
+    out = 1 / (1 + np.exp(-z))
     return out
 
-a1 = (np.dot(wl1, x)) + bl1
+a1 = (np.matmul(wl1, x)) + bl1
 zl1 = (sigmoide(a1))
 print (f"Salida layer 1: {zl1}")
 
-a2 = (np.dot(wl2, zl1)) + bl2
+a2 = (np.matmul(wl2, zl1)) + bl2
 zl2 = (sigmoide(a2))
 print (f"Salida layer 2: {zl2}")
 
-error = (zl2 - y)
-print (f"Error (delta z): {error}")
 
 '''Actualizar pesos 2'''
+error2 = (zl2 - y)
+print (f"Error (delta z2): {error2}")
 '''salida layer 1 traspuesta'''
 zl1T = zl1.T
-errorpesos2 = zl1T * error
-wl2 = wl2 - alpha * errorpesos2
+#errorpesos2 = zl1T * error
+errorpesos2 = np.dot(error2,zl1T)
+
+wl2 =  wl2 - alpha * errorpesos2
 print (f"Pesos 2 actualizado: {wl2}")
+
+
+'''Actualizar pesos 1'''
+parte1 = np.dot(wl2.T, error2)
+parte2 = zl1 * (1 - zl1)
+
+deltas1 = parte1 * parte2
+print (f"Deltas layer1: {deltas1}")
+
+'''x traspuesta'''
+xT = x.T
+errorpesos1 = np.dot(deltas1,xT)
+
+wl1 = wl1 - alpha * errorpesos1
+print( f"Pesos 1 actualizado: {wl1}")
+
 
