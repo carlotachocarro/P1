b391ab47d195935463af2290edb140ad3492edd9
carlotachocarro <carlota.chocarro@students.salle.url.edu>
2024-12-05 11:18:50 +0100

    exercici 1: part 2

--- perceptron.2.py
+++ perceptron.2.py
@@ -7,6 +7,8 @@ x = np.ones ([2,1])
 x[0, 0] = 0
 x[1, 0] = 1
 y = np.ones ([2,1])
+y[0, 0] = 0
+y[1, 0] = 1
 bl1 = np.ones ([3, 1])
 a1 = np.ones ([3,1])
 wl2 = np.ones([2, 3])
@@ -23,37 +25,39 @@ def sigmoide(z):
     out = 1 / (1 + np.exp(-z))
     return out
 
-a1 = (np.matmul(wl1, x)) + bl1
-zl1 = (sigmoide(a1))
-print (f"Salida layer 1: {zl1}")
+for i in range (30):
 
-a2 = (np.matmul(wl2, zl1)) + bl2
-zl2 = (sigmoide(a2))
-print (f"Salida layer 2: {zl2}")
+    a1 = (np.matmul(wl1, x)) + bl1
+    zl1 = (sigmoide(a1))
+    print (f"Salida layer 1: {zl1}")
 
-
-'''Actualizar pesos 2'''
-error2 = (zl2 - y)
-print (f"Error (delta z2): {error2}")
-'''salida layer 1 traspuesta'''
-zl1T = zl1.T
-#errorpesos2 = zl1T * error
-errorpesos2 = np.dot(error2,zl1T)
-
-wl2 =  wl2 - alpha * errorpesos2
-print (f"Pesos 2 actualizado: {wl2}")
+    a2 = (np.matmul(wl2, zl1)) + bl2
+    zl2 = (sigmoide(a2))
+    print (f"Salida layer 2: {zl2}")
 
 
-'''Actualizar pesos 1'''
-parte1 = np.dot(wl2.T, error2)
-parte2 = zl1 * (1 - zl1)
+    '''Actualizar pesos 2'''
+    error2 = (zl2 - y)
+    print (f"Error (delta z2): {error2}")
+    '''salida layer 1 traspuesta'''
+    zl1T = zl1.T
+    #errorpesos2 = zl1T * error
+    errorpesos2 = np.dot(error2,zl1T)
 
-deltas1 = parte1 * parte2
-print (f"Deltas layer1: {deltas1}")
+    wl2 =  wl2 - alpha * errorpesos2
+    print (f"Pesos 2 actualizado: {wl2}")
 
-'''x traspuesta'''
-xT = x.T
-errorpesos1 = np.dot(deltas1,xT)
 
-wl1 = wl1 - alpha * errorpesos1
-print( f"Pesos 1 actualizado: {wl1}")
+    '''Actualizar pesos 1'''
+    parte1 = np.dot(wl2.T, error2)
+    parte2 = zl1 * (1 - zl1)
+
+    deltas1 = parte1 * parte2
+    print (f"Deltas layer1: {deltas1}")
+
+    '''x traspuesta'''
+    xT = x.T
+    errorpesos1 = np.dot(deltas1,xT)
+
+    wl1 = wl1 - alpha * errorpesos1
+    print( f"Pesos 1 actualizado: {wl1}")
